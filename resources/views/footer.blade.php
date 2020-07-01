40ef55c7e64e0871f5979b41a255a4bff2c14aa0
Kunal Aaryen Sinha <aaryen11@outlook.com>
2020-07-01 21:02:12 +0530

    Footer Files

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<!--
+    Author : Kunal Aaryen Sinha
+    Mob. No. : 8709162797
+-->
+<link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+<div class="footer">
+  <p class="pt-2">Copyright &copy; <?php echo date('Y');?> | All Rights Reserved</p>
+</div>
