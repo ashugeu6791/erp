8d19624bf0198ea4af062011f7292ccad2b8143e
Kunal Aaryen Sinha <aaryen11@outlook.com>
2020-07-02 20:22:39 +0530

    Welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img src="https://i.gifer.com/NW2o.gif" >
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('login') }}" style="font-size:20px">Login</a>
+                    <a href="https://www.geu.ac.in" style="font-size:20px">Website</a>
                 </div>
             </div>
         </div>
