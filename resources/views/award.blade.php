b384b92bbed044df276c42ba88597545b7d31212
Kunal Aaryen Sinha <aaryen11@outlook.com>
2020-07-09 21:13:39 +0530

    Update award.blade.php

--- resources/views/award.blade.php
+++ resources/views/award.blade.php
@@ -24,96 +24,114 @@
 @stop
 <link rel="stylesheet" href="{{asset('css/exam.css')}}">
 <div class="main_content">
-    <div class="info" style="margin-left:3%;margin-right:3%;margin-top:5%">
-    <h4 class="pb-3"><b>Upload Award List</b></h4>
-    <form class="form-inline">  
-            <div class="form-group pb-4"> 
-                <label for="university" class="col-form-label text-md-right pr-3">University</label> 
-                <div class="col-md-6 pl-4">
-                            <select id="university" class=" form-control" style="width:580px;" name = 'university' required autofocus>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8" style="padding-top:4%">
+            <div class="card shadow p-3 mb-5 bg-white round">
+                <div class="card-body">
+                    <form method="POST" action=" ">
+                        @csrf
+
+                        <div class="form-group row" style="padding-top:5%">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('University') }}</label>
+
+                            <div class="col-md-6">
+                            <select id="university" class=" form-control" name = 'university' required autofocus>
                             <option hidden disabled selected value></option>
                                 <option value="1">Graphic Era (Deemed to be University)</option>
                                 <option value="2">Graphic Era Hill University</option>
                                 <option value="3">Graphic Era Hill University - Bhimtal</option>
                             </select>
-                </div>
-            </div>  
-            <div class="form-group pb-4"> 
-                <label for="exam" class="pl-4 col-form-label text-md-right pr-4">Exam Type</label> 
-                <div class="col-md-6 pl-4">
-                            <select id="exam" class=" form-control" style="width:580px" name = 'exam' required autofocus>
-                            <option hidden disabled selected value></option>
-                                <option value="1">X</option>
-                                <option value="2">Y</option>
-                                <option value="3">Z</option>
-                            </select>
-                </div> 
-            </div>
-            <div class="form-group pb-4"> 
-                <label for="program" class="col-form-label text-md-right pr-4">Program</label> 
-                <div class="col-md-6 pl-4">
-                            <select id="program" class=" form-control" style="width:580px" name = 'program' required autofocus>
-                            <option hidden disabled selected value></option>
-                                <option value="1">X</option>
-                                <option value="2">Y</option>
-                                <option value="3">Z</option>
-                            </select>
-                </div>
-            </div>  
-            <div class="form-group pb-4"> 
-                <label for="branch" class="pl-4 col-form-label text-md-right pr-5">Branch</label> 
-                <div class="col-md-6 pl-4">
-                            <select id="branch" class=" form-control" style="width:580px" name = 'branch' required autofocus>
-                            <option hidden disabled selected value></option>
-                                <option value="1">X</option>
-                                <option value="2">Y</option>
-                                <option value="3">Z</option>
-                            </select>
-                </div> 
-            </div>
-            <div class="form-group pb-4"> 
-                <label for="code" class="col-form-label text-md-right pr-1">Subject Code</label> 
-                <div class="col-md-6 ">
-                            <select id="code" onChange="return func()" class=" form-control" style="width:580px" name = 'code' required autofocus>
-                            <option hidden disabled selected value></option>
-                                <option value="1">X</option>
-                                <option value="2">Y</option>
-                                <option value="3">Z</option>
-                            </select>
-                </div>
-            </div>
-            <script>
-                    function func(){
-                    var dropdown = document.getElementById("code");
-                    var selection = dropdown.value;
-                    console.log(selection);
-                    var TextBox = document.getElementById("name");
-                    TextBox.value = selection;
-                         }
-             </script>
-        
-            <div class="form-group pb-4"> 
-                <label for="name" class="pl-4 col-form-label text-md-right pr-1">Subject Name</label> 
-                <div class="col-md-6 pl-4">
-                    <input id="name" class=" form-control" style="width:580px" name = 'name' disabled required autofocus>
-                </div> 
-            </div>   
-    </form>
+                         </div>
+                        </div>
 
-            <div id="FileUpload">
-                <div class="wrapper">
-                    <div class="upload">
-                        <p><span class="upload__button" onclick="chooseFile();"> 
-                        <input type="file" id="fileInput" name="myfile" /><i class="fas fa-paperclip"></i> Upload Files
-                        </span></p>
+                        <div class="form-group row">
+                            <label for="designation" class="col-md-4 col-form-label text-md-right">{{ __('Exam Type') }}</label>
+
+                            <div class="col-md-6">
+                            <select id="exam" class=" form-control" name = 'exam' required autofocus>
+                            <option hidden disabled selected value></option>
+                                <option value="1">X</option>
+                                <option value="2">Y</option>
+                                <option value="3">Z</option>
+                            </select>
+                            </div> 
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Program') }}</label>
+
+                            <div class="col-md-6">
+                            <select id="program" class=" form-control" name = 'program' required autofocus>
+                            <option hidden disabled selected value></option>
+                                <option value="1">X</option>
+                                <option value="2">Y</option>
+                                <option value="3">Z</option>
+                            </select>
+                             </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="eligibility" class="col-md-4 col-form-label text-md-right">{{ __('Subject Code') }}</label>
+
+                            <div class="col-md-6 ">
+                            <select id="code" onChange="return func()" class=" form-control" name = 'code' required autofocus>
+                            <option hidden disabled selected value></option>
+                                <option value="1">X</option>
+                                <option value="2">Y</option>
+                                <option value="3">Z</option>
+                            </select>
+                        </div>
+                        </div>
+
                         <script>
-                        function chooseFile() {
-                            document.getElementById("fileInput").click();
-                        }
+                            function func(){
+                            var dropdown = document.getElementById("code");
+                            var selection = dropdown.value;
+                            console.log(selection);
+                            var TextBox = document.getElementById("name");
+                            TextBox.value = selection;
+                                }
                         </script>
-                    </div>
+
+                        <div class="form-group row">
+                            <label for="package" class="col-md-4 col-form-label text-md-right">{{ __('Package') }}</label>
+
+                            <div class="col-md-6">
+                            <input id="name" class=" form-control" name = 'name' disabled required autofocus>
+                             </div>
+                        </div>
+
+                        <div id="FileUpload">
+                            
+                                <div class="upload">
+                                <p><span class="upload__button" onclick="chooseFile();"> 
+                                <input type="file" id="fileInput" name="myfile" /><i class="fas fa-paperclip"></i> Upload Files
+                                </span></p>
+                                <script>
+                                function chooseFile() {
+                                    document.getElementById("fileInput").click();
+                                }
+                                </script>
+                                </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Add') }}
+                                </button>
+                                <button type="Reset" class="btn btn-primary">
+                                    {{ __('Clear') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
 <div>
     <?php echo View::make('footer') ?>
 </div>
